506d9e99197337b973115ef6becc6cdfd6efe441
paguist1 <160607996+paguist1@users.noreply.github.com>
2024-04-20 17:21:19 -0400

    proyecto con css y js
    entrega final del proyecto Wordle

--- script.js
+++ script.js
@@ -1,83 +1,54 @@
-console.log("Hola Mundo") //console.log "imprime" la variable que se encuentra dentro de () en la consola
+let intentos = 6;
+let diccionario = ["PIEZA","PUERTA","TEXTO", "RATON", "MANTA", "MENTA"];
 
-//desafio comparar 3 numeros y determinar cual es el mayor.
-// n1 n2 n3 si n1 > n2 > n3 -> n1 es el mayor 
-let n1 = 5 //se usa let para agregar y asignar una variable
-let n2 = 8
-let n3 = 15
-if(n1 === n2 === n3){ //usar triple = indica igualdad estricta ===
-console.log("son todos iguales")
+const button = document.getElementById("guess-button");
+button.addEventListener("click", intentar);
+
+function obtenerPalabra (){
+    let aleatorio= Math.floor(Math.random()*diccionario.length)
+    return diccionario[aleatorio]
 }
-else if (n1 > n3 && n1 > n2){ //&& indica "y"
-    mayor= n1
-}else if(n2 > n3 && n2 > n3){
-    mayor= n2
-}else if (n3 > n2 && n3 > n2){
-    mayor= n3
+
+let palabraOculta = obtenerPalabra()
+console.log (palabraOculta)
+
+function intentar(){
+    const INTENTO = document.getElementById("guess-input").value.toUpperCase()
+    intentos --
+    if (intentos== 0 ){
+        terminar ("PERDISTE!")
+    } 
+    if (palabraOculta == INTENTO){
+        terminar ("GANASTE!")
+        
+    }else{
+        let fila = document.createElement("div")
+        fila.className = "row"
+        console.log(fila)
+        for (const i in INTENTO){
+            let letra = document.createElement("span")
+            letra.className = "letter"
+            letra.innerText = INTENTO [i]
+            fila.appendChild(letra)
+            console.log(fila)
+            if (INTENTO[i] == palabraOculta[i]){
+                letra.style.background = "#59D978"
+            }else if (palabraOculta.includes(INTENTO[i])){
+                letra.style.background = "#DBD62B"
+            } else {
+                letra.style.background = "#707454"
+            }
+        }
+    let grilla = document.getElementById("grid")
+    grilla.appendChild(fila)
 }
-console.log(mayor, "es el mayor numero")
 
-//funciones
-//SUMAR
-
-// function Suma(n1, n2, n3){
-//     let resultado;
-//     resultado = n1 + n2 + n3
-//     console.log("la suma de los tres numeros es", resultado)
-// }
-// Suma(2,3,10)
-
-// function Comparar (n1, n2, n3){
-// if (n1 > n3 && n1 > n2)
-// mayor = n1
-// else if (n2 > n3 && n2 > n1)
-// mayor = n2
-// else if ( n3 > n1 && n3 > n2)
-// mayor = n3
-// console.log(mayor, "es el mayor numero")
-// return mayor 
-// }
-// Comparar (2, 5, 10)
-// Suma(mayor, 2, 2)
-// console.log("Hola")
-// let intentos = 6;
-// let palabra = "APPLE"; //Vector o array, el array es una lista de elemetos, se escriben entre corchete y entre comas. Los elementos puede ser de tipò nuemro y de tipo texto
-// let usuario = "MOUSE"
-// let vector = ["A","P","P","L","E"]
-// if( usuario === palabra){
-//     console.log(Ganaste)
-// }else {
-//     intentos = intentos - 1;
-//     console.log("te quedan", intentos)
-//     for (i in palabra) {
-//         if (palabra [i] === usuario[i]){
-//             console.log(usuario [i], "verde")
-//         }else if (palabra.includes(usuario[i])){
-//             console.log(usuario[i], "amarillo")
-//         }else{
-//             console.log(usuario[i], "gris")
-//         }
-
-//     }
-// }
-// function Intentar(intento){
-//     if (intento === palabra){
-//         console.log(ganaste)
-//     }else {
-//         intentos = intento - 1;
-//         console.log("te quedan", intentos, "intentos");
-//         for (i in palabra){
-//             if(palabra [i]=== intento [i]){
-//                 console.log(intento[i], "verde")
-//             }else if (palabra.includes[intento[i]]){
-//                 console.log(intento[i], "amarillo")
-//                 }else{
-//                     console.log(intento[i], "gris")
-//                 }
-//             }
-//         }
-//     }
-//     console.log["fin de bucle"]
-//     if [intentos=== 0](
-//         console.log("Perdiste")
-//     )
+function terminar(mensaje){
+    const input = document.getElementById("guess-input");
+    input.disabled = true;
+    button.disabled = true;
+    let p = document.getElementById("guesses");
+    p.innerHTML = "<h1>" + mensaje + "</h1>"
+    
+}
+}
